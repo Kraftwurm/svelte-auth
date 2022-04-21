b333bb74ec698ee761cba4393d7dbbb56b642fa8
Kraftwurm <kraftwurm@gmail.com>
2022-04-22 01:46:06 +0200

    add cloudflare adapter

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,10 @@
 import preprocess from 'svelte-preprocess';
+import cloudflare from '@sveltejs/adapter-cloudflare';
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+	kit: {
+		adapter: cloudflare(),	
+	},
 	preprocess: [
 		preprocess({
 			postcss: true,
