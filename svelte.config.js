52f94fdd82dd7060065c100b602fdc76a5444652
Kraftwurm <kraftwurm@gmail.com>
2022-04-22 02:10:42 +0200

    test

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,14 @@
 import preprocess from 'svelte-preprocess';
 //import adapter from '@sveltejs/adapter-static';
 import adapter from '@sveltejs/adapter-cloudflare';
+import vercel from '@sveltejs/adapter-vercel';
 //import adapter from '@sveltejs/adapter-node';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	kit: {
 		//adapter: adapter({ out: 'build' }),
-		adapter: adapter(),
+		adapter: vercel({ edge : false, external: [], split: false})
 		//adapter: adapter(),
 		// hydrate the <div id="svelte"> element in src/app.html
 
